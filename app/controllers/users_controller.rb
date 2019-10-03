1de80fd17362712dd23b3a7fe8470bda179bb8d3
Cristian Garcia <eminemko@hotmail.com>
2019-10-02 22:11:57 -0500

    add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,6 +18,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
     @user = User.find(params[:id])
   end
 
